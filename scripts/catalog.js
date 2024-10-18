861f453180e3a6ccd6c9cf1529a16542d617dd5d
Ezequiel Vallejos Lugo <eze0072@hotmail.com>
2024-10-18 15:23:30 -0300

    cambio de estilos de filtro

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,36 +1,35 @@
 /**********************Carga de productos******************************/
 // Lista de productos (agregué los materiales como un array en cada producto)
 const products = [
-    { name: "Remera Vent Verde", price: 15, img: "assets/catalog/remera-1.png", link: "producto.html", category: "remeras", materials: ["algodon"] },
-    { name: "Bolso CICA Neumático negro", price: 35, img: "assets/catalog/bolso.png", link: "producto.html", category: "accesorios", materials: ["camaras"] },
-    { name: "Jerséi de lana reciclada", price: 25, img: "assets/catalog/jersei.png", link: "producto.html", category: "remeras", materials: ["lana"] },
-    { name: "Sneaker Spandau Navy Man", price: 30, img: "assets/catalog/zapatillas-1.png", link: "producto.html", category: "calzado", materials: ["poliester"] },
-    { name: "Pantalones Cargo Ethica Azules", price: 27, img: "assets/catalog/pantalones.png", link: "producto.html", category: "pantalones", materials: ["algodon", "poliester"] },
-    { name: "Remera azul", price: 15, img: "assets/catalog/remera-azul.png", link: "producto.html", category: "remeras", materials: ["algodon"] },
-    { name: "Camiseta Francisca blanca", price: 15, img: "assets/catalog/musculosa.png", link: "producto.html", category: "remeras", materials: ["algodon"] },
-    { name: "Sneaker Corsocomo Woman", price: 30, img: "assets/catalog/zapatillas-2.png", link: "producto.html", category: "calzado", materials: ["poliester"] },
-    { name: "Bandolera Frodo L", price: 30, img: "assets/catalog/bandolera.png", link: "producto.html", category: "accesorios", materials: ["camaras"] },
-    { name: "Pantalones cortos Beige", price: 20, img: "assets/catalog/bermuda.png", link: "producto.html", category: "pantalones", materials: ["algodon", "lana"] },
+    { name: "Remera Vent Verde", price: 15000, img: "assets/catalog/remera-1.png", link: "producto.html", category: "remeras", materials: ["algodon"] },
+    { name: "Bolso CICA Neumático negro", price: 35000, img: "assets/catalog/bolso.png", link: "producto.html", category: "accesorios", materials: ["camaras"] },
+    { name: "Jerséi de lana reciclada", price: 25000, img: "assets/catalog/jersei.png", link: "producto.html", category: "remeras", materials: ["lana"] },
+    { name: "Sneaker Spandau Navy Man", price: 30000, img: "assets/catalog/zapatillas-1.png", link: "producto.html", category: "calzado", materials: ["poliester"] },
+    { name: "Pantalones Cargo Ethica Azules", price: 27000, img: "assets/catalog/pantalones.png", link: "producto.html", category: "pantalones", materials: ["algodon", "poliester"] },
+    { name: "Remera azul", price: 15000, img: "assets/catalog/remera-azul.png", link: "producto.html", category: "remeras", materials: ["algodon"] },
+    { name: "Camiseta Francisca blanca", price: 15000, img: "assets/catalog/musculosa.png", link: "producto.html", category: "remeras", materials: ["algodon"] },
+    { name: "Sneaker Corsocomo Woman", price: 30000, img: "assets/catalog/zapatillas-2.png", link: "producto.html", category: "calzado", materials: ["poliester"] },
+    { name: "Bandolera Frodo L", price: 30000, img: "assets/catalog/bandolera.png", link: "producto.html", category: "accesorios", materials: ["camaras"] },
+    { name: "Pantalones cortos Beige", price: 20000, img: "assets/catalog/bermuda.png", link: "producto.html", category: "pantalones", materials: ["algodon", "lana"] },
 ];
 
 // Selección del catálogo donde se mostrarán los productos
 const catalog = document.querySelector('.product-list');
-
 let selectedCategory = null;
 let selectedPrice = null;
 let selectedMaterials = [];
 
 /**********************Renderizado de productos******************************/
 function renderProducts(filteredProducts) {
-    catalog.innerHTML = ''; // Limpiar catálogo antes de renderizar
+    catalog.innerHTML = ''; // Limpia el catálogo antes de renderizar
     if (filteredProducts.length === 0) {
-        catalog.innerHTML = '<p>No hay resultados</p>'; // Mostrar mensaje si no hay productos
+        catalog.innerHTML = '<p>No hay resultados</p>'; // Muestra el mensaje si no hay productos
         return;
     }
     filteredProducts.forEach(product => {
         const productDiv = document.createElement('div');
         productDiv.classList.add('product');
-        productDiv.setAttribute('data-category', product.category); // Agregar categoría como atributo
+        productDiv.setAttribute('data-category', product.category); // Agrega la categoría como atributo
         productDiv.innerHTML = `
             <a href="${product.link}">
                 <img src="${product.img}" alt="${product.name}">
@@ -75,10 +74,16 @@ categoryLinks.forEach(link => {
     link.addEventListener('click', function (e) {
         e.preventDefault(); // Prevenir la acción predeterminada del enlace
 
+        // Remueve la clase 'selected' de todos los enlaces
+        categoryLinks.forEach(link => link.classList.remove('selected'));
+
+        // Agrega la clase 'selected' al enlace clickeado
+        this.classList.add('selected');
+
         // Obtiene la categoría seleccionada
         selectedCategory = link.textContent.trim().toLowerCase();
 
-        // Reiniciar el filtro de precio y material
+        // Reinicia el filtro de precio y material
         selectedPrice = null;
         selectedMaterials = [];
         priceRange.value = priceRange.max; // Restablecer el control de rango de precios
