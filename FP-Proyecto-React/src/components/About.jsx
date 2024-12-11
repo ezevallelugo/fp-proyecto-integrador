08df2d011e7db3c9121c03c1b01f4a662e0d226b
Leandro <leandroviscolungo@gmail.com>
2024-12-10 23:22:24 -0300

    en progreso checkpoint

---
+++ FP-Proyecto-React/src/components/About.jsx
@@ -0,0 +1,134 @@
+export default function About() {
+    return (
+        <div>
+
+
+            <body>
+
+                <div class="container-first">
+                    <div class="first-box fade-in">
+                        <div class="logo-first">
+                            <img src="assets/about/logo-completo.png" alt="Logo Ecodono">
+                        </div>
+                        <h1>¿QUIÉNES SOMOS?</h1>
+                        <p>Somos una plataforma dedicada a impulsar la economía circular, ofreciendo un espacio donde las personas pueden <strong>comprar, vender, reciclar</strong> y dar nueva vida a productos de segunda mano. Creemos en el poder de la <strong>reutilización</strong> y el <strong>upcycling</strong> para reducir el impacto ambiental y crear un cambio positivo.</p>
+                        <a href="#history" class="btn-cta">Explora más</a>
+                    </div>
+                </div>
+
+                <section class="features">
+                    <div class="feature fade-in">
+                        <img src="assets/about/mundo.png" alt="Reciclaje">
+                            <p>Promovemos el reciclaje y reutilización para reducir el impacto ambiental.</p>
+                    </div>
+                    <div class="feature fade-in">
+                        <img src="assets/about/simbolo.png" alt="Conectar personas">
+                            <p>Conectando personas para un futuro sostenible.</p>
+                    </div>
+                    <div class="feature fade-in">
+                        <img src="assets/about/bote.png" alt="Soluciones sustentables">
+                            <p>Inspirando soluciones sustentables para un mundo mejor.</p>
+                    </div>
+                </section>
+
+                <section class="history fade-in">
+                    <h2 id="history">NUESTRA HISTORIA</h2>
+                    <p>
+                        Todo comenzó con una vieja cubierta de auto. Al principio, parecía un residuo más destinado al olvido, una pieza más en el rompecabezas de la contaminación. Pero nosotros vimos su potencial. De esa cubierta, surgió un par de zapatillas que no solo eran funcionales y cómodas, sino que también contaban una historia: la historia de lo que se puede lograr con imaginación y compromiso.
+                        Nos dimos cuenta de que en cada objeto que otros consideraban basura, había una oportunidad esperando ser aprovechada. Así nació nuestra plataforma, con la idea de que cada residuo, sin importar cuán pequeño o grande, puede tener un nuevo propósito. No solo queríamos vender productos, sino crear una comunidad que compartiera esta visión. Desde ese primer par de zapatillas recicladas, nos propusimos demostrar que lo que consideramos "desechos" en realidad puede ser la base de un futuro más sostenible. Hoy, nuestra plataforma conecta a personas que desean hacer del mundo un lugar mejor, un intercambio a la vez.
+                    </p>
+                </section>
+
+
+                <section class="services fade-in">
+                    <h2>¿QUÉ OFRECEMOS?</h2>
+                    <div class="service">
+                        <img src="assets/about/economia.png" alt="Economía circular real">
+                            <h3>Economía circular <br> real</h3>
+                            <p>Integramos reciclaje y upcycling, transformando productos en desuso en algo nuevo.</p>
+                    </div>
+                    <div class="service">
+                        <img src="assets/about/reduccion.png" alt="Reducción del desperdicio">
+                            <h3>Reducción del desperdicio</h3>
+                            <p>Reciclamos materiales como plástico, goma y textiles para crear productos útiles.</p>
+                    </div>
+                    <div class="service"><img src="assets/about/comunidad.png" alt="Comunidad comprometida">
+                        <h3>Comunidad comprometida</h3>
+                        <p>Fomentamos un espacio donde las personas pueden vender, donar o comprar productos.</p>
+                    </div>
+                </section>
+
+                <section class="team fade-in">
+                    <h2>EL EQUIPO DETRÁS DE ESTE PROYECTO</h2>
+                    <div class="team-members">
+                        <div class="member feature fade-in">
+                            <div class="circle">
+                                <img src="assets/about/ezequiel.jpg" alt="Foto de Ezequiel">
+                            </div>
+                            <p>Ezequiel Vallejos</p>
+                        </div>
+                        <div class="member feature fade-in">
+                            <div class="circle">
+                                <img src="assets/about/aldana.jpg" alt="Foto de Aldana">
+                            </div>
+                            <p>Aldana Rolon</p>
+                        </div>
+                        <div class="member feature fade-in">
+                            <div class="circle">
+                                <img src="assets/about/julieta.jpg" alt="Foto de Julieta">
+                            </div>
+                            <p>Julieta Ojeda</p>
+                        </div>
+                        <div class="member feature fade-in">
+                            <div class="circle">
+                                <img src="assets/about/axel.jpg" alt="Foto de Axel">
+                            </div>
+                            <p>Axel Appella</p>
+                        </div>
+                        <div class="member feature fade-in">
+                            <div class="circle">
+                                <img src="assets/about/leandro.jpg" alt="Foto de Leandro">
+                            </div>
+                            <p>Leandro Viscolungo</p>
+                        </div>
+                        <div class="member feature fade-in">
+                            <div class="circle">
+                                <img src="assets/about/tamara.jpg" alt="Foto de Tamara">
+                            </div>
+                            <p>Tamara Vera</p>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="statistics fade-in">
+                    <h2>DATOS Y ESTADÍSTICAS QUE DEMUESTRAN UN MUNDO MÁS VERDE</h2>
+                    <div class="stats-container">
+                        <div class="stat-item">
+                            <img src="assets/about/rueda.png" alt="Reciclaje de neumáticos">
+                                <h3 id="stat1">150000</h3>
+                                <p><strong>Neumáticos reciclados por año</strong> <br> a través de la reutilización de caucho.</p>
+                        </div>
+                        <div class="stat-item">
+                            <img src="assets/about/agua.png" alt="Ahorro de agua">
+                                <h3 id="stat2">4.5 mil millones</h3>
+                                <p><strong>Litros de agua ahorrados</strong> <br> en la producción de nuevas prendas.</p>
+                        </div>
+                        <div class="stat-item">
+                            <img src="assets/about/vertedero.png" alt="Impacto ambiental">
+                                <h3 id="stat3">20 millones</h3>
+                                <p><strong>Toneladas de residuos</strong> <br> NO terminaron en vertederos.</p>
+                        </div>
+                        <div class="stat-item">
+                            <img src="assets/about/co2.png" alt="C02 reducido">
+                                <h3 id="stat4">70</h3>
+                                <p><strong>CO2 reducido</strong> <br> en la producción de nuevas prendas.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <script src="scripts/about-script.js"></script>
+            </body>
+
+        </div>
+    );
+}
